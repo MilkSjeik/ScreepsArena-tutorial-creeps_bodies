d725f2e17de63d62dbbbe4addaef932003f09159
Stijn Mertens <stijn.mertens@expertum.net>
2022-04-09 14:21:06 +0200

    Initial solution

--- main.mjs
+++ main.mjs
@@ -1,8 +1,32 @@
-import { } from '/game/utils';
-import { } from '/game/prototypes';
-import { } from '/game/constants';
-import { } from '/arena';
+import { getObjectsByPrototype } from '/game/utils';
+import { Creep } from '/game/prototypes';
+import { ERR_NOT_IN_RANGE, ATTACK, HEAL, RANGED_ATTACK } from '/game/constants';
 
 export function loop() {
-    // Your code goes here
+    const creeps =  getObjectsByPrototype(Creep);
+    const myCreeps = creeps.filter(creep => creep.my);
+    const myDamagedCreeps = myCreeps.filter(i => i.my && i.hits < i.hitsMax);
+    const enemyCreep = creeps.find(creep => !creep.my);
+
+    myCreeps.forEach(myCreep => {
+        console.log(myCreep.body);
+        if(myCreep.body.some(bodyPart => bodyPart.type == ATTACK)) {
+            if(myCreep.attack(enemyCreep) == ERR_NOT_IN_RANGE) {
+                myCreep.moveTo(enemyCreep);
+            }
+        }
+        if(myCreep.body.some(bodyPart => bodyPart.type == HEAL)) {
+            if(myDamagedCreeps.length > 0) {
+                const damagedCreep = myDamagedCreeps[0];
+                if(myCreep.rangedHeal(damagedCreep) == ERR_NOT_IN_RANGE) {
+                    myCreep.moveTo(damagedCreep);
+                }
+            }
+        }
+        if(myCreep.body.some(bodyPart => bodyPart.type == RANGED_ATTACK)) {
+            if(myCreep.rangedAttack(enemyCreep) == ERR_NOT_IN_RANGE) {
+                myCreep.moveTo(enemyCreep);
+            }
+        }
+    });
 }
